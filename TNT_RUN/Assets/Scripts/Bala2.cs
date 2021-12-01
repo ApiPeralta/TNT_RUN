4998627d4045b705c229a442ebce87013f449acc
ApiPeralta <69977662+ApiPeralta@users.noreply.github.com>
2021-12-01 09:22:40 -0300

    Cañon2 corregido

--- TNT_RUN/Assets/Scripts/Bala2.cs
+++ TNT_RUN/Assets/Scripts/Bala2.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bala : MonoBehaviour
+public class Bala2 : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player2"))
         {
             StartCoroutine(CoolDown());
         }
     }
     IEnumerator CoolDown()
     {
-        Player_Movement.walkSpeed = 1.5f;
+        Player_Joystick.walkSpeed = 1.5f;
         yield return new WaitForSeconds(2);
-        Player_Movement.walkSpeed = 3.0f;
+        Player_Joystick.walkSpeed = 3.0f;
     }
 }
